93a01aea4f0dc26272bf97b2446eb236eec2f843
sb1769203-ui <sb1769203@gmail.com>
2026-08-30 15:52:10 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -39,37 +39,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
-// Einheitlicher App-Ton: überall derselbe kurze, einfache Beep.
-function playAppSound() {
-  try {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    if (!AudioContext) return;
-
-    if (!window.__appAudioContext) {
-      window.__appAudioContext = new AudioContext();
-    }
-
-    const ctx = window.__appAudioContext;
-    const start = () => {
-      const osc = ctx.createOscillator();
-      const gain = ctx.createGain();
-
-      osc.type = "sine";
-      osc.frequency.setValueAtTime(700, ctx.currentTime);
-      gain.gain.setValueAtTime(0.045, ctx.currentTime);
-      gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.08);
-
-      osc.connect(gain);
-      gain.connect(ctx.destination);
-      osc.start();
-      osc.stop(ctx.currentTime + 0.08);
-    };
-
-    if (ctx.state === "suspended") {
-      ctx.resume().then(start).catch(() => {});
-    } else {
-      start();
-    }
-  } catch (_) {}
-}
